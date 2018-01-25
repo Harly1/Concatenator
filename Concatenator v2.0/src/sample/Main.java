c9e56f07f13048290292fd5608e5ec06ec50baa6
harly <harly1@yandex.ru>
2018-01-25 14:57:07 +0300

    Проверка 3

--- Concatenator v2.0/src/sample/Main.java
+++ Concatenator v2.0/src/sample/Main.java
@@ -10,6 +10,7 @@ import sample.controllers.Controller;
 public class Main extends Application {
 // Проверка ГИТ!!!
 // Проверка 2 XXX!!!!!!!
+// Проверка 3
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader();
